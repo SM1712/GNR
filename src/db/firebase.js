13080f533d52970868f1e80c466794de8b30b3e3
Sebastian Mesones <mesones536@gmail.com>
2026-05-26 22:31:50 -0500

    security: Mover credenciales de Firebase a variables de entorno (.env) ignoradas por Git

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -18,12 +18,12 @@ import {
 import { getAllRecords, bulkAddRecords, clearAllRecords } from './indexedDB';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRtTCYnFKDErByr46AXpHIyeKSObMgIMs",
-  authDomain: "grf-fosma.firebaseapp.com",
-  projectId: "grf-fosma",
-  storageBucket: "grf-fosma.firebasestorage.app",
-  messagingSenderId: "56359289522",
-  appId: "1:56359289522:web:f7423d5888273af34d3fab"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
